7db10f68ef6433a56bcabb300ceb6f5c53e399d9
Gareth Uren <garethyoo@gmail.com>
2024-10-20 17:02:14 +0200

    initial commit

---
+++ src/main/java/com/yootiful/thymleafhtmx/ThymleafHtmxApplication.java
@@ -0,0 +1,13 @@
+package com.yootiful.thymleafhtmx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymleafHtmxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymleafHtmxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yootiful/thymleafhtmx/ThymleafHtmxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yootiful.thymleafhtmx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymleafHtmxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
